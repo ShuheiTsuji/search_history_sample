102e0e7915c93b01ef3fc729944fee9cb8f6db28
ShuheiTsuji <shuhei.tsuji@icloud.com>
2018-01-17 18:36:39 +0900

    step1 :smile:

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
   def index
+    @id      = params[:id]
+    @name    = params[:name]
+    @address = params[:address]
+    @sex     = params[:sex]
+
+    @users = User.all
+    @users = @users.where(id: @id) if @id.present?
+    @users = @users.where('name like ?', "%#{@name}%") if @name.present?
+    @users = @users.where(address: @address) if @address.present?
+    @users = @users.where(sex: @sex) if @sex.present
   end
 end
