1ac80386c3e07c3220734f68f8c5f8af8e7b9068
ShuheiTsuji <shuhei.tsuji@icloud.com>
2018-01-17 15:55:34 +0900

    slim導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+  enum sex: %i( man woman )
 end
--- db/migrate/20180117063337_create_users.rb
+++ db/migrate/20180117063337_create_users.rb
@@ -1,6 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
+      t.string :name, null: false
+      t.string :zip
+      t.string :address
+      t.integer :sex, null: false, limit: 1
 
       t.timestamps
     end
