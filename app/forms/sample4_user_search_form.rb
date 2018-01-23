b42adbf3bcef4b1db9fd6396b06a63bfaa6e92c2
ShuheiTsuji <shuhei.tsuji@icloud.com>
2018-01-23 17:11:33 +0900

    sample4_user 歴史4追加

---
+++ app/forms/sample4_user_search_form.rb
@@ -0,0 +1,27 @@
+class Sample4UserSearchForm
+  include ActiveModel::Model
+  include ActiveModelAttributes
+
+  attribute :id, :integer
+  attribute :name, :string
+  attribute :zip, :zip
+  attribute :sex, :integer
+
+  validates :id, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 100 }, allow_blank: true
+  validates :name, length: { maximum: 100 }
+
+  def search
+    User.search(params_for_search)
+  end
+
+  private
+
+  def params_for_search
+    {
+      id: id,
+      name: name,
+      zip: zip,
+      sex: sex
+    }
+  end
+end
