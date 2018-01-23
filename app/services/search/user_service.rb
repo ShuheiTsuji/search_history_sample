62d4b36e6c17e04e10725c696f44f224ee9e6a66
ShuheiTsuji <shuhei.tsuji@icloud.com>
2018-01-23 17:13:15 +0900

    既存のごちゃ混ぜにしていた記述全て削除

--- app/services/search/user_service.rb
+++ app/services/search/user_service.rb
@@ -2,7 +2,7 @@
 # service層利用スタイル
 module Search
   class UserService
-    attr_reader :user_activerecord_relation
+    attr_reader :relation
 
     def initialize(user_form)
       @user_form = user_form
@@ -23,25 +23,25 @@ module Search
     def set_id
       return if @user_form.id.blank?
 
-      @relation = @relation.where(id: @user_form.id)
+      @relation.where!(id: @user_form.id)
     end
 
     def set_name
       return if @user_form.name.blank?
 
-      @relation = @relation.where('name like ?', "%#{@user_form.name}%")
+      @relation.where!('name like ?', "%#{@user_form.name}%")
     end
 
     def set_zip
       return if @user_form.zip.blank?
 
-      @relation = @relation.where(zip: @user_form.zip)
+      @relation.where!(zip: @user_form.zip)
     end
 
     def set_sex
       return if @user_form.sex.blank?
 
-      @relation = @relation.where(sex: @user_form.sex)
+      @relation.where!(sex: @user_form.sex)
     end
   end
 end
