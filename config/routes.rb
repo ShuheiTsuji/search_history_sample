a85f49e050ed7d044f4550f7a8567c0c1f87e7bd
ShuheiTsuji <shuhei.tsuji@icloud.com>
2018-01-23 16:45:34 +0900

    sample_user４つ分のrouting作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users, only: %i[index]
+
+  resource :sample1_users, only: %i[show]
+  resource :sample2_users, only: %i[show]
+  resource :sample3_users, only: %i[show]
+  resource :sample4_users, only: %i[show]
 end
