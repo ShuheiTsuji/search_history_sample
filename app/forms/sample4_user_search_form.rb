4ff6262e3803e6a1d90f284fca59fe55f83547b6
ryoff <ryoffes@gmail.com>
2018-01-24 14:47:23 +0900

    Set less_than_or_equal_to to 100_000_000 in UserSearchForm#id

--- app/forms/sample4_user_search_form.rb
+++ app/forms/sample4_user_search_form.rb
@@ -7,7 +7,7 @@ class Sample4UserSearchForm
   attribute :zip, :zip
   attribute :sex, :integer
 
-  validates :id, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 100 }, allow_blank: true
+  validates :id, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 100_000_000 }, allow_blank: true
   validates :name, length: { maximum: 100 }
 
   def search
