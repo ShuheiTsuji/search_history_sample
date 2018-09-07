0f2e685b4f9668408bb6d4b121aea6386132a0f0
ShuheiTsuji <shuhei.tsuji.sfc@gmail.com>
2018-09-07 15:10:34 +0900

    add top_page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  root :to => 'welcome#index'
   resource :sample1_users, only: %i[show]
   resource :sample2_users, only: %i[show]
   resource :sample3_users, only: %i[show]
